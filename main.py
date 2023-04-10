6d73beb734cf2642d3a2754a96b3c4ce5f99cd72
BuildTools <unconfigured@null.spigotmc.org>
2023-04-09 21:26:58 -0700

    game over screen

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ JUMP_VELOCITY = -10
 # Set up variables
 player_vel_y = 0
 jump_sound = pygame.mixer.Sound('sounds/jump.mp3')
-game_active = True
+
 # Set up the game window
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
@@ -35,11 +35,11 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-player_surface = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
+player_surface = pygame.image.load('graphics/Player/player128x128.png').convert_alpha()
 
 mystery_block_surface = pygame.image.load('graphics/mystery_block.png').convert_alpha()
-mystery_block_rectangle = mystery_block_surface.get_rect(midbottom=(0, 268))
-
+mystery_block_rectangle = mystery_block_surface.get_rect(midbottom=(80, 300))
+screen.blit(mystery_block_surface, mystery_block_rectangle)
 
 # Load game sounds
 
@@ -55,12 +55,14 @@ snail_rectangle = snail_surface.get_rect(bottomright = (600,300))
 
 game_running = True
 # Game loop
-while  True:
+while game_running == True:
 
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            time.sleep(100000)
             pygame.quit()
+            time.sleep(100000)
             exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
@@ -77,9 +79,6 @@ while  True:
                 player_vel_y = JUMP_VELOCITY
         if event.type == pygame.KEYUP:
             pass
-        if event.type == pygame.MOUSEMOTION:
-            mouse_x, mouse_y = event.pos
-            print(f"Mouse is hovering at ({mouse_x}, {mouse_y})")
 
     # Update player position based on velocity
     player_rectangle.y += player_vel_y
@@ -90,28 +89,31 @@ while  True:
         player_rectangle.bottom = 300
         player_vel_y = 0
 
-    if game_active == True:
-        screen.blit(sky_surface,(0,0)) 
-        screen.blit(ground_surface,(0,300))
-        screen.blit(text_surface,(300,50))
+    # Render game graphics
+    screen.blit(sky_surface,(0,0)) 
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
     if player_rectangle.colliderect(mystery_block_rectangle):
        coin_sound.play()
        coin_rectangle = coin_surface.get_rect(center=mystery_block_rectangle.center)
-       screen.blit(coin_surface, coin_rectangle) # Update snail position
-       snail_rectangle.x -= 1
-    if snail_rectangle.right <= 0: 
-       snail_rectangle.left = 800
-         # Render snail and player rectangles
-       screen.blit(snail_surface,snail_rectangle)
-       screen.blit(player_surface,player_rectangle)
-       screen.blit(mystery_block_surface, mystery_block_rectangle)
-       
-    # Check for collisions
-        #if snail_rectangle.colliderect(player_rectangle):
-           #pygame.quit
-           #exit()
+       screen.blit(coin_surface, coin_rectangle)
 
-  
+    # Update snail position
+    snail_rectangle.x -= 4
+    if snail_rectangle.right <= 0: 
+        snail_rectangle.left = 800
+    
+    # Render snail and player rectangles
+    screen.blit(snail_surface,snail_rectangle)
+    screen.blit(player_surface,player_rectangle)
+   
+    # Check for collisions
+    if snail_rectangle.colliderect(player_rectangle):
+       screen.fill((0, 0, 0))
+       game_running = True
+       screen.blit(game_over_text, game_over_rect)
+
     # Update the game display
     pygame.display.update()
     
