9859c35e3332cb81b7609a66c2c9c988781a3929
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 18:46:59 -0400

    main menu

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sys import exit
 from pygame.locals import *
 from pygame import mixer
 import time
+import button 
 
 pygame.init()
 font = pygame.font.SysFont("font/", 60)
@@ -31,7 +32,7 @@ JUMP_VELOCITY = -10
 # Set up variables
 player_vel_y = 0
 jump_sound = pygame.mixer.Sound('sounds/jump.mp3')
-
+TEXT_COL = (255, 255, 255)
 game_paused = False
 menu_state = "main"
 font = pygame.font.SysFont("arialblack", 40)
@@ -41,6 +42,68 @@ SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Runner')
+
+#load button images
+resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
+options_img = pygame.image.load("images/button_options.png").convert_alpha()
+quit_img = pygame.image.load("images/button_quit.png").convert_alpha()
+video_img = pygame.image.load('images/button_video.png').convert_alpha()
+audio_img = pygame.image.load('images/button_audio.png').convert_alpha()
+keys_img = pygame.image.load('images/button_keys.png').convert_alpha()
+back_img = pygame.image.load('images/button_back.png').convert_alpha()
+
+#create button instances
+resume_button = button.Button(304, 125, resume_img, 1)
+options_button = button.Button(297, 250, options_img, 1)
+quit_button = button.Button(336, 375, quit_img, 1)
+video_button = button.Button(226, 75, video_img, 1)
+audio_button = button.Button(225, 200, audio_img, 1)
+keys_button = button.Button(246, 325, keys_img, 1)
+back_button = button.Button(332, 450, back_img, 1)
+
+
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
+
+#check if game is paused
+  if game_paused == True:
+    #check menu state
+    if menu_state == "main":
+      #draw pause screen buttons
+      if resume_button.draw(screen):
+        game_paused = False
+      if options_button.draw(screen):
+        menu_state = "options"
+      if quit_button.draw(screen):
+        run = False
+    #check if the options menu is open
+    if menu_state == "options":
+      #draw the different options buttons
+      if video_button.draw(screen):
+        print("Video Settings")
+      if audio_button.draw(screen):
+        print("Audio Settings")
+      if keys_button.draw(screen):
+        print("Change Key Bindings")
+      if back_button.draw(screen):
+        menu_state = "main"
+  else:
+    draw_text("Press SPACE to pause", font, TEXT_COL, 160, 250)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Set up the game clock and font
 clock = pygame.time.Clock()
